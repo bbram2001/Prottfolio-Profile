222b2155d6547f962cef8540d63d9b59a6a995c3
bbram2001 <rambharath997@gmail.com>
2024-07-22 23:25:24 +0530

    add portfolio file

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new typed(".text", {
+    Strings : ["Forntend Developer" , "Java Developer" , "Web Developer"],
+
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+
+    loop: true
+});
